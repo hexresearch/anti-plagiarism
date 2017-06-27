9c2ef04d76a7954268e665494e05af4fd70bc8de
vonavi <ivvl82@gmail.com>
2017-06-27 17:30:55 +0300

    Output the Jaccard coefficient in tests. Closes #1

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,12 +3,14 @@ module Main
     main
   ) where
 
+import           Data.Function         (on)
 import qualified Data.Text             as T
 import qualified Data.Text.IO          as T
 import           System.Directory      (listDirectory)
 import           System.FilePath.Posix ((</>))
 import           System.Random         (getStdGen)
 
+import           Data.HXR.AP.LSH       (TextRep (..), hashedShingles, kJaccard)
 import           Plagiarisms           (plagiarisms)
 import           Types                 (Options (..), TextOrder (..))
 import           Utils                 (breakByEmptyLine, breakToSentences)
@@ -19,12 +21,14 @@ main = do
   texts <- listDirectory dir >>= mapM (T.readFile . (dir </>))
 
   gen <- getStdGen
-  let opt = Options
-            { breakToPar   = breakByEmptyLine
-            , breakToElem  = breakToSentences
-            , textOrder    = Original
-            , maxNumOfElem = 20
-            , maxNumOfPar  = 5
-            , randElemGen  = gen
-            }
-  putStrLn . T.unpack $ plagiarisms opt texts
+  let opt    = Options
+               { breakToPar   = breakByEmptyLine
+               , breakToElem  = breakToSentences
+               , textOrder    = Original
+               , maxNumOfElem = 20
+               , maxNumOfPar  = 5
+               , randElemGen  = gen
+               }
+      pieces = plagiarisms opt texts
+  putStr "Jaccard: "
+  print $ (kJaccard `on` hashedShingles . LshTextK9) (T.concat texts) pieces
